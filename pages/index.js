f9a460d75e34911cdf79571a44009f2c73567e92
FrameTee29 <teeraphat123@hotmail.com>
2020-03-22 13:12:07 +0700

    input email password

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+
 const Index=()=>{
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
     return(
         <div>
-            Hello
+            <h1>JWT Token with React</h1>
+            <div>
+                <input type="text" onChange={(e)=>setEmail(e.target.value)} />
+                <input type="text" onChange={(e)=>setPassword(e.target.value)} />
+            </div>
+            {email} : {password}
         </div>
     )
 }
