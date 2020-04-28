f6395bd488934bd5f264c1e52f1d9946acb8d226
FrameTee29 <teeraphat123@hotmail.com>
2020-04-28 19:33:45 +0700

    ...

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4532fbbdeb7f103a10e7";
+/******/ 	var hotCurrentHash = "91e672ba0ff6ac2538a6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
