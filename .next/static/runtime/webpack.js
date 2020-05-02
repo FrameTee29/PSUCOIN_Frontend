d8c4a3eff3dd849acc71181cf3bdc49177266ac1
FrameTee29 <teeraphat123@hotmail.com>
2020-05-02 14:08:43 +0700

    npm install @material-ui/core

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "91e672ba0ff6ac2538a6";
+/******/ 	var hotCurrentHash = "aaa81213827afedae6af";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
