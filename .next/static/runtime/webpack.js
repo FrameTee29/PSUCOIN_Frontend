49ee992e7ce2a838b5adf7389ae3fedbeb508fae
FrameTee29 <teeraphat123@hotmail.com>
2020-07-03 20:00:38 +0700

    next to responsive login page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5a9cd0371a481265ff2b";
+/******/ 	var hotCurrentHash = "631c6a711a7ab3bcaeaf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
